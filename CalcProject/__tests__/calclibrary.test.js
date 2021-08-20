9e9b62f94651ea2473ff1efc31fdf316411efb05
sagar-aryal <sagar_yal92@outlook.com>
2021-08-20 13:04:20 +0300

    testing with floating numbers and parameters

--- CalcProject/__tests__/calclibrary.test.js
+++ CalcProject/__tests__/calclibrary.test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { sum } = require("../calclibrary");
+const { sum, subtract } = require("../calclibrary");
 
 describe("Test sum with integers", () => {
   test("test 1+1=2", () => {
@@ -29,5 +29,141 @@ describe("Test sum with integer using test.each", () => {
 
   test.each(testValues)("sum(%s,%s) = %s", (a, b, expected) => {
     expect(sum(a, b)).toBe(expected);
+  });
+});
+
+describe("Testing sum with floating point numbers", () => {
+  const testValues = [
+    [10, 11.5, 21.5],
+    [2.5, 3, 5.5],
+    [-2.5, 3, 0.5],
+    [3, -2.5, 0.5],
+    [-3, 2.5, -0.5],
+    [-2.5, -3, -5.5],
+    [-2.5, -2.5, -5.0],
+    [-2.5, 2.5, 0],
+    [2.4, -2.5, -0.1],
+  ];
+
+  test.each(testValues)("sum(%s, %s) = %s", (a, b, expected) => {
+    expect(sum(a, b)).toBeCloseTo(expected);
+  });
+});
+
+describe("Test missing parameters throw an expection", () => {
+  test('sum() throws an expection "parameter missing"', () => {
+    expect(() => sum()).toThrow("parameter missing");
+  });
+
+  const testValues = [
+    [null, "parameter missing"],
+    [1, "parameter missing"],
+    ["a", "parameter missing"],
+    ["", "parameter missing"],
+  ];
+
+  test.each(testValues)(
+    "sum(%s) throws an expection: %s",
+    (value, expected) => {
+      expect(() => sum(value)).toThrow(expected);
+    }
+  );
+});
+
+describe("Testing parameters are not numbers", () => {
+  const testValues = [
+    ["a", 1, "only numbers are allowed"],
+    [1, "a", "only numbers are allowed"],
+    ["a", "b", "only numbers are allowed"],
+    ["", "", "only numbers are allowed"],
+    [null, 1, "parameter missing"],
+    [undefined, 1, "parameter missing"],
+  ];
+  test.each(testValues)("sum(%s, %s) throws %s", (a, b, expected) => {
+    expect(() => sum(a, b)).toThrow(expected);
+  });
+});
+
+///////// SUBTRACTION /////////
+
+describe("Test differences with integers", () => {
+  test("test 1-1=0", () => {
+    expect(subtract(1, 1)).toBe(0);
+  });
+
+  test("test 2-3=-1", () => {
+    expect(subtract(2, 3)).toBe(-1);
+  });
+});
+
+describe("Test difference with integer using test.each", () => {
+  const testValues = [
+    //a b expected
+    [1, 1, 0],
+    [2, 3, -1],
+    [-2, -4, 2],
+    [-2, 4, -6],
+    [2, -4, 6],
+    [0, 0, 0],
+    [0, 3, -3],
+    [3, 0, 3],
+    [0, -3, 3],
+    [-3, 0, -3],
+  ];
+
+  test.each(testValues)("subtract(%s,%s) = %s", (a, b, expected) => {
+    expect(subtract(a, b)).toBe(expected);
+  });
+});
+
+describe("Testing difference with floating point numbers", () => {
+  const testValues = [
+    [10, 11.5, -1.5],
+    [2.5, 3, -0.5],
+    [-2.5, 3, -5.5],
+    [3, -2.5, 5.5],
+    [-3, 2.5, -5.5],
+    [-2.5, -3, 0.5],
+    [-2.5, -2.5, 0],
+    [-2.5, 2.5, -5],
+    [2.4, -2.5, 4.9],
+  ];
+
+  test.each(testValues)("subtract(%s, %s) = %s", (a, b, expected) => {
+    expect(subtract(a, b)).toBeCloseTo(expected);
+  });
+});
+
+describe("Test missing parameters throw an expection", () => {
+  test('subtract() throws an expection "parameter missing"', () => {
+    expect(() => subtract()).toThrow("parameter missing");
+  });
+
+  const testValues = [
+    [null, "parameter missing"],
+    [1, "parameter missing"],
+    ["a", "parameter missing"],
+    ["", "parameter missing"],
+  ];
+
+  test.each(testValues)(
+    "subtract(%s) throws an expection: %s",
+    (value, expected) => {
+      expect(() => subtract(value)).toThrow(expected);
+    }
+  );
+});
+
+describe("Testing parameters are not numbers", () => {
+  const testValues = [
+    ["a", 1, "only numbers are allowed"],
+    [1, "a", "only numbers are allowed"],
+    ["a", "b", "only numbers are allowed"],
+    ["", "", "only numbers are allowed"],
+    [null, 1, "parameter missing"],
+    [undefined, 1, "parameter missing"],
+  ];
+  test.each(testValues)("subtract(%s, %s) throws %s", (a, b, expected) => {
+    expect(() => subtract(a, b)).toThrow(expected);
   });
 });
--- CalcProject/calcAPI.md
+++ CalcProject/calcAPI.md
@@ -24,4 +24,91 @@ Returns the sum on given numbers.
   [0, 3, 3],
   [3, 0, 3],
   [0,- 3, -3],
-  [-3, 0, -3]
+  [-3, 0, -3],
+
+##### Floats
+
+sum(10,11.5) returns 21.5
+
+[10, 11.5, 21.5],
+[2.5, 3, 5.5],
+[-2.5, 3, 0.5],
+[3, -2.5, 0.5],
+[-3, 2.5, -0.5],
+[-2.5, -3, -5.5],
+[-2.5, -2.5, -5.0],
+[-2.5, 2.5, 0],
+[2.4, -2.5, -0.1]
+
+#### Missing Parameters
+
+- sum() throws an expection `'parameter missing'`
+- sum(1) throws an expection `'parameter missing'`
+- sum('a') throws an expection `'parameter missing'`
+- sum('') throws an expection `'parameter missing'`
+
+#### Parameters are not numbers
+
+- sum('a', 1) throws an expection `'only numbers are allowed'`
+- sum(1, 'a') throws an expection `'only numbers are allowed'`
+- sum('a', 'b') throws an expection `'only numbers are allowed'`
+- sum('', '') throws an expection `'only numbers are allowed'`
+- sum(null, 1) throws an expection `'parameter missing'`
+- sum(undefined, 1) throws an expection `'parameter missing'`
+
+## **subtract(a,b)**
+
+Function returns the differences of two numbers. Numbers are passed to the function as parameters.
+
+Returns the differences on given numbers.
+
+- if parameter is missing, throw an exception: `'parameter missing'`
+- if parameters are not numbers, throw an exception: `'only numbers are allowed'`
+
+### Test cases
+
+#### 1 subtract
+
+##### Integers
+
+- subtract(1,1) returns 0
+- subtract(4,3) returns 1
+- subtract(-2,4) returns -6
+- subtract(2,4) returns -2
+  [2, -4, -2],
+  [0, 0, 0],
+  [0, 3, 3],
+  [3, 0, 3],
+  [0,- 3, -3],
+  [-3, 0, -3],
+
+##### Floats
+
+- subtract(10,11.5) returns -1.5
+- subtract(5.5,2.5) returns 3
+
+[10, 11.5, 21.5],
+[2.5, 3, 5.5],
+[-2.5, 3, 0.5],
+[3, -2.5, 0.5],
+[-3, 2.5, -0.5],
+[-2.5, -3, -5.5],
+[-2.5, -2.5, -5.0],
+[-2.5, 2.5, 0],
+[2.4, -2.5, -0.1]
+
+#### Missing Parameters
+
+- subtract() throws an expection `'parameter missing'`
+- subtract(1) throws an expection `'parameter missing'`
+- subtract('a') throws an expection `'parameter missing'`
+- subtract('') throws an expection `'parameter missing'`
+
+#### Parameters are not numbers
+
+- subtract('a', 1) throws an expection `'only numbers are allowed'`
+- subtract(1, 'a') throws an expection `'only numbers are allowed'`
+- subtract('a', 'b') throws an expection `'only numbers are allowed'`
+- subtract('', '') throws an expection `'only numbers are allowed'`
+- subtract(null, 1) throws an expection `'parameter missing'`
+- subtract(undefined, 1) throws an expection `'parameter missing'`
